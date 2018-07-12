d4b0986a486667dff718d394f7aac7106073ef84
cmcclellan1010 <cmcclellan1010@gmail.com>
2018-07-12 10:35:00 -0600

    renaming and organization

---
+++ dendrocat/mastercatalog.py
@@ -0,0 +1,124 @@
+import radiosource
+from astropy.table import MaskedColumn
+import numpy as np
+import utils
+import astropy.units as u
+
+
+class MasterCatalog:
+    """
+    An object to store combined data from two or more RadioSource objects.
+    """
+    
+    def __init__(self, *args, catalog=None):
+        """
+        Create a new master catalog object.
+        
+        Parameters
+        ----------
+        catalog : astropy.table.Table object
+            The master table from which to build the catalog object.
+        *args : radiosource.RadioSource objects
+            RadioSource objects from which the master table was built.
+        
+        NOTE: Currently only supports multiple objects if they're of different
+              frequencies (the freq_id must be unique).
+        """
+        if catalog is not None:
+            self.catalog = catalog
+        
+        obj_prefix = 'radiosource_'
+        
+        for obj in args:
+        
+            if isinstance(obj, MasterCatalog):
+                for key in obj.__dict__.keys():
+                    if key.split('_')[0]+'_' == obj_prefix:
+                        self.__dict__[key] = obj[key]
+            else:        
+                self.__dict__[obj_prefix+obj.freq_id] = obj
+
+    
+    def photometer(self, aperture, catalog=None, **kwargs):
+        """
+        Add photometry data columns to the master catalog.
+        
+        Parameters
+        ----------
+        aperture : utils aperture mask function
+            The function that creates a mask in the shape of the desired
+            aperture, given source information and a cutout
+        catalog : astropy.table.Table object
+            The catalog from which to extract source coordinates and ellipse
+            parameters.
+        """
+        if catalog is None:
+            catalog = self.catalog
+        
+        rs_objects = []
+        for i, obj in enumerate(self.__dict__.values()):
+            if isinstance(obj, radiosource.RadioSource):
+                rs_objects.append(obj)
+                
+        aperture_npix_col = MaskedColumn(length=len(catalog),
+                                         name=aperture.__name__,
+                                         mask=True)
+        
+        for i, rs_obj in enumerate(rs_objects):
+        
+            size = 2.2*(np.max(catalog['major_fwhm'])*u.deg 
+                        + rs_obj.annulus_padding 
+                        + rs_obj.annulus_width)
+                        
+            cutouts, cutout_data = rs_obj._make_cutouts(size, 
+                                                        catalog=catalog, 
+                                                        save=False)
+        
+            try:
+                pix_in_aperture = rs_obj.__dict__['pix_'+aperture.__name__]
+            except KeyError:                              
+                pix_in_aperture = rs_obj.get_pixels(
+                                                    aperture,
+                                                    cutouts=cutouts,
+                                                    cutout_data=cutout_data,
+                                                    **kwargs
+                                                    )
+            
+            names = [
+                aperture.__name__+'_peak_'+rs_obj.freq_id,
+                aperture.__name__+'_sum_'+rs_obj.freq_id,
+                aperture.__name__+'_rms_'+rs_obj.freq_id,
+                aperture.__name__+'_median_'+rs_obj.freq_id
+            ]
+            
+            peak_data = np.amax(np.amax(pix_in_aperture, axis=1), axis=1)
+            aperture_peak_col = MaskedColumn(data=peak_data,
+                                             name=names[0],
+            
+                                             mask=True)
+            sum_data = np.sum(np.sum(pix_in_aperture, axis=1), axis=1)                         
+            aperture_sum_col = MaskedColumn(data=sum_data,
+                                            name=names[1],
+                                            mask=True)                  
+                                            
+            rms_data = np.zeros(len(pix_in_aperture))
+            for i in range(len(pix_in_aperture)):
+                rms_data[i] = utils.rms(pix_in_aperture[i])                           
+            aperture_rms_col = MaskedColumn(data=rms_data,
+                                            name=names[2],
+                                            mask=True)
+            
+            median_data = np.median(np.median(pix_in_aperture, axis=1), axis-1)                   
+            aperture_median_col = MaskedColumn(data=median_data,
+                                               name=names[3],
+                                               mask=True)
+            
+            self.catalog.add_columns([
+                aperture_peak_col,
+                aperture_sum_col,
+                aperture_rms_col,
+                aperture_median_col
+            ])
+            
+            
+            
---
+++ dendrocat/utils.py
@@ -0,0 +1,241 @@
+import numpy as np
+from radio_beam import Beams
+import astropy.units as u
+from astropy.table import MaskedColumn, vstack
+from astropy.utils.console import ProgressBar
+from copy import deepcopy
+import mastercatalog
+import regions
+import warnings
+import pickle
+
+def mask(reg, cutout):
+    n = cutout.shape[0]
+    mask = reg.to_mask(mode='center')
+    return np.array(mask.to_image((n, n)), dtype='int')
+    
+def rms(x):
+    return (np.absolute(np.mean(x**2) - (np.mean(x))**2))**0.5
+
+def check_units(quantity, unit=u.deg):
+    if unit.is_equivalent(quantity):
+        return quantity.to(unit)
+    else:
+        return quantity * u.Unit(unit)
+        warnings.warn("Assuming quantity is in {}".format(unit))
+
+def commonbeam(major1, minor1, pa1, major2, minor2, pa2):
+    """
+    Create a smallest bounding ellipse around two other ellipses. 
+    Give ellipse dimensions as astropy units quantities.
+    """
+    major1 = check_units(major1, unit=u.deg)
+    minor1 = check_units(minor1, unit=u.deg)
+    pa1 = check_units(pa1, unit=u.deg)
+    major2 = check_units(major2, unit=u.deg)
+    minor2 = check_units(minor2, unit=u.deg)
+    pa2 = check_units(pa2, unit=u.deg)
+    
+    somebeams = Beams([major1.to(u.arcsec), major2.to(u.arcsec)]*u.arcsec, 
+                      [minor1.to(u.arcsec), minor2.to(u.arcsec)]*u.arcsec, 
+                      [pa1, pa2]*u.deg)
+                      
+    common = somebeams.common_beam()
+    new_major = common._major
+    new_minor = common._minor
+    new_pa = common._pa
+    
+    return new_major.to(u.deg), new_minor.to(u.deg), new_pa
+
+def save_regions(catalog, outfile, hide_rejects=True):
+    """
+    Save a catalog as a a DS9 region file.
+    
+    Parameters
+    ----------
+    catalog : astropy.table.Table, RadioSource, or MasterCatalog object
+        The catalog or catalog-containing object from which to extract source
+        coordinates and ellipse properties.
+    outfile : str
+        Path to save the region file.
+    hide_rejects : bool, optional
+        If enabled, rejected sources will not be saved. Default is True
+    """
+    
+    if outfile.split('.')[-1] != 'reg':
+        warnings.warn('Invalid or missing file extension. Self-correcting.')
+        outfile = outfile.split('.')[0]+'.reg'
+    
+    if hide_rejects:
+        try:
+            catalog = self.catalog[np.where(self.catalog['rejected'] == 0)]
+        except AttributeError:
+            catalog = catalog[np.where(catalog['rejected'] == 0)]
+    else:
+        try:
+            catalog = self.catalog
+        except AttributeError:
+            pass
+            
+    with open(outfile, 'w') as fh:
+        fh.write("icrs\n")
+        for row in catalog:
+            fh.write("ellipse({x_cen}, {y_cen}, {major_fwhm}, " \
+                     "{minor_fwhm}, {position_angle}) # text={{{_idx}}}\n"
+                     .format(**dict(zip(row.colnames, row))))
+
+def match(*args, verbose=True):
+    """
+    Documentation needed
+    """
+    current_arg = args[0]
+    for i in range(len(args)-1):
+        current_arg = _matcher(current_arg, args[i+1])
+    return current_arg
+
+def _matcher(obj1, obj2, verbose=True):
+    """
+    Find sources that match up between two radio objects. 
+    
+    Parameters
+    ----------
+    obj1 : rsprocess.RadioSource object or rsprocess.MasterCatalog object
+        A catalog with which to compare radio sources.
+    obj2 : rsprocess.RadioSource object or rsprocess.MasterCatalog object
+        A catalog with which to compare radio sources.
+        
+    Returns
+    ----------
+    rsprocess.MasterCatalog object
+    """
+  
+    all_colnames = set(obj1.catalog.colnames + obj2.catalog.colnames)
+    stack = vstack([obj1.catalog, obj2.catalog])
+    
+    all_colnames.add('_idy')
+    stack.add_column(Column(range(len(stack)), name='_idy'))
+    stack = stack[sorted(list(all_colnames))]
+    
+    rejected = np.where(stack['rejected'] == 1)[0]
+    
+    if verbose:
+        print('Combining matches')
+        pb = ProgressBar(len(stack) - len(rejected))
+    
+    i = 0
+    while True:
+        
+        if i == len(stack) - 1:
+            break
+        
+        if i in rejected:
+            i += 1
+            continue
+        
+        teststar = stack[i]
+        delta_p = vstack([stack[:i], stack[i+1:]]
+                         )['_idx', '_idy', 'x_cen', 'y_cen']
+        delta_p['x_cen'] = np.abs(delta_p['x_cen'] - teststar['x_cen'])                
+        delta_p['y_cen'] = np.abs(delta_p['y_cen'] - teststar['y_cen'])
+        delta_p.sort('x_cen')
+        
+        threshold = 1e-5
+        found_match = False
+        
+        dist_col = MaskedColumn(length=len(delta_p), name='dist', 
+                                mask=True)
+        
+        for j in range(10):
+            dist_col[j] = np.sqrt(delta_p[j]['x_cen']**2. 
+                                  + delta_p[j]['y_cen']**2)            
+            if dist_col[j] <= threshold:
+                found_match = True
+                
+        delta_p.add_column(dist_col)
+        delta_p.sort('dist')
+        
+        if found_match:
+            match_index = np.where(stack['_idy'] == delta_p[0]['_idy'])
+            match = deepcopy(stack[match_index])
+            stack.remove_row(match_index[0][0])
+            
+            # Find the common bounding ellipse
+            new_x_cen = np.average([match['x_cen'], teststar['x_cen']])
+            new_y_cen = np.average([match['y_cen'], teststar['y_cen']])
+            
+            # Find new ellipse properties
+            new_maj, new_min, new_pa = commonbeam(
+                                         float(match['major_fwhm']), 
+                                         float(match['minor_fwhm']), 
+                                         float(match['position_angle']),
+                                         float(teststar['major_fwhm']),
+                                         float(teststar['minor_fwhm']),
+                                         float(teststar['position_angle'])
+                                         )
+            
+            # Replace properties of test star
+            stack[i]['x_cen'] = new_x_cen       
+            stack[i]['y_cen'] = new_y_cen
+            stack[i]['major_fwhm'] = new_maj.value
+            stack[i]['minor_fwhm'] = new_min.value
+            stack[i]['position_angle'] = new_pa.value
+    
+            # Replace masked data with available values from the match
+            for k, masked in enumerate(stack.mask[i]):
+                colname = stack.colnames[k]
+                if masked:
+                    stack[i][colname] = match[colname]
+        i += 1
+        if verbose:
+            pb.update()
+    
+    # Fill masked detection column fields with 'False'
+    for colname in stack.colnames:
+        if colname.split('_')[0] == 'detected':
+            stack[colname].fill_value = 0
+    
+    stack.remove_column('_idy')
+    return mastercatalog.MasterCatalog(obj1, obj2, catalog=stack)
+    
+    
+# APERTURE MASK FUNCTIONS
+def ellipse(source, cutout, obj):
+
+    center = regions.PixCoord(cutout.center_cutout[0], cutout.center_cutout[1])
+
+    pix_major = source['major_fwhm']*u.deg / obj.pixel_scale
+    pix_minor = source['minor_fwhm']*u.deg / obj.pixel_scale
+    pa = source['position_angle']*u.deg
+    
+    radius = source['major_fwhm'] * u.deg
+    reg = regions.EllipsePixelRegion(center, pix_major, pix_minor, angle=pa)
+                     
+    ellipse_mask = mask(reg, cutout)
+    return ellipse_mask
+
+
+def annulus(source, cutout, obj):
+    
+    center = regions.PixCoord(cutout.center_cutout[0], cutout.center_cutout[1])
+    inner_r = source['major_fwhm']*u.deg + obj.annulus_padding
+    outer_r = inner_r + obj.annulus_width
+    
+    innerann_reg = regions.CirclePixelRegion(center, inner_r/obj.pixel_scale)
+    outerann_reg = regions.CirclePixelRegion(center, outer_r/obj.pixel_scale)
+    
+    annulus_mask = (mask(outerann_reg, cutout) - mask(innerann_reg, cutout))
+    return annulus_mask
+
+
+def circle(source, cutout, obj, radius=None):
+        
+    if radius is None:
+        radius = source['major_fwhm'] * u.deg
+    else:
+        radius = check_units(radius, unit=u.deg)
+    
+    center = regions.PixCoord(cutout.center_cutout[0], cutout.center_cutout[1])
+    reg = regions.CirclePixelRegion(center, radius/obj.pixel_scale)
+    
+    circle_mask = mask(reg, cutout)
+    return circle_mask

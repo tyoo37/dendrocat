b0736f3df8116827e1fb2f4a802fe762d664abad
cmcclellan1010 <cmcclellan1010@gmail.com>
2018-07-23 09:30:57 -0600

    added photometry test script

---
+++ dendrocat/tests/photometry.py
@@ -0,0 +1,16 @@
+import dendrocat
+from astropy.io import fits
+import astropy.units as u
+
+rs1 = dendrocat.RadioSource(fits.open('/lustre/aoc/students/bmcclell/w51/W51e2_cont_briggsSC_tclean.image.fits.gz'))
+rs2 = dendrocat.RadioSource(fits.open('/lustre/aoc/students/bmcclell/w51/w51e2_sci.spw0_1_2_3_4_5_6_7_8_9_10_11_12_13_14_15_16_17_18_19.mfs.I.manual.image.tt0.pbcor.fits.gz'))
+rs3 = dendrocat.RadioSource(fits.open('/lustre/aoc/students/bmcclell/w51/W51e2w_QbandAarray_cont_spws_continuum_cal_clean_2terms_robust0_incrementalselfcal8.image.tt0.pbcor.icrs.fits'))
+
+rs3.nu = 45*u.GHz
+rs3.freq_id = '45Ghz'
+rs3.set_metadata()
+
+rs1.autoreject()
+rs2.autoreject()
+
+mc = dendrocat.MasterCatalog(rs1, rs2, rs3, catalog=dendrocat.match(rs1, rs2).catalog)
